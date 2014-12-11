8b5ca90e166e20fc93e1af7ca4f213825139f767
Alex Crichton <alex@alexcrichton.com>
2014-12-11 10:36:48 -0800

    Trim down the crate attribute listings

--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,6 @@
 
 //! Support code for encoding and decoding types.
 
-/*
-Core encoding and decoding interfaces.
-*/
-
-#![crate_type = "rlib"]
-#![crate_type = "dylib"]
-#![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
-       html_favicon_url = "http://www.rust-lang.org/favicon.ico",
-       html_root_url = "http://doc.rust-lang.org/nightly/",
-       html_playground_url = "http://play.rust-lang.org/")]
-#![allow(unknown_features)]
 #![feature(macro_rules, default_type_params, slicing_syntax, globs)]
 
 // test harness access
