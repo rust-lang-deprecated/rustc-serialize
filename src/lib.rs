c92250ed5011a450cb54c96605ff1cbde3e95b75
Alex Crichton <alex@alexcrichton.com>
2015-03-25 08:40:32 -0700

    Bump to 0.3.7

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,12 @@
 
 //! Support code for encoding and decoding types.
 
-#![feature(core, convert)]
+#![feature(convert)]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/rustc-serialize/")]
 #![cfg_attr(test, deny(warnings))]
-#![cfg_attr(test, feature(test))]
+#![cfg_attr(test, feature(test, std_misc))]
 
 #[cfg(test)] extern crate test;
 #[cfg(test)] extern crate rand;
