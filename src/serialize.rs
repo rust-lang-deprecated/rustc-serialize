db777e859b7237ee1e4c06f6561cffd03c2d4e97
Simon Wollwage <mail.wollwage@gmail.com>
2015-03-25 11:11:59 +0900

    changed to PathBuf::from and added convert feature

--- src/serialize.rs
+++ src/serialize.rs
@@ -570,18 +570,14 @@ impl Decodable for path::PathBuf {
         use std::os::unix::prelude::*;
         let bytes: Vec<u8> = try!(Decodable::decode(d));
         let s: OsString = OsStringExt::from_vec(bytes);
-        let mut p = path::PathBuf::new();
-        p.push(s);
-        Ok(p)
+        Ok(path::PathBuf::from(s))
     }
     #[cfg(windows)]
     fn decode<D: Decoder>(d: &mut D) -> Result<path::PathBuf, D::Error> {
         use std::os::windows::prelude::*;
         let bytes: Vec<u16> = try!(Decodable::decode(d));
         let s: OsString = OsStringExt::from_wide(&bytes);
-        let mut p = PathBuf::new();
-        p.push(s);
-        Ok(p)
+        Ok(path::PathBuf::from(s))
     }
 }
 
