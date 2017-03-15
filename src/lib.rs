7a93906bab419369ba52e899085b3131cb74cbc3
Nick Cameron <ncameron@mozilla.com>
2017-03-15 15:39:42 +1300

    Add rustbuild boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,9 @@
 //! extern crate rustc_serialize;
 //! ```
 
+#![cfg_attr(rustbuild, feature(staged_api, rustc_private))]
+#![cfg_attr(rustbuild, unstable(feature = "rustc_private", issue = "27812"))]
+
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "https://www.rust-lang.org/favicon.ico",
        html_root_url = "https://doc.rust-lang.org/rustc-serialize/")]
